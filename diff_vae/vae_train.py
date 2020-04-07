a04e9734e3e91a8d96687ac14e2eee13828f3849
Vlad Gainullin <gainullin@gmail.com>
2020-04-06 21:48:50 -0400

    environment file

--- diff_vae/vae_train.py
+++ diff_vae/vae_train.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../fast_jtnn/')
 import torch
 import torch.nn as nn
 import torch.optim as optim
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 import torch
 import torch.nn as nn
 from multiprocessing import Pool
